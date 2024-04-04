4cad58b671996e67a3dc7b9150495132139b323c
RXGabriel <gabriel2005rf@gmail.com>
2024-04-04 12:24:06 -0300

    test: Ensure getCharactersFromXML method returns a list of Character entities from RickAndMortyUSA integration

---
+++ test/integrations/rickAndMortyUSA.test.js
@@ -0,0 +1,18 @@
+import { expect, describe, test, jest, beforeEach } from '@jest/globals'
+import axios from 'axios'
+import fs from 'fs/promises'
+import RickAndMortyUSA from '../../src/business/integrations/rickAndMortyUSA.js'
+import Character from '../../src/entities/character.js'
+
+describe('#RickAndMortyUSA', () => {
+    beforeEach(() => jest.clearAllMocks())
+    test('#getCharactersFromXML should return a list of Character Entity', async () => {
+        const response = await fs.readFile('./test/mocks/characters.xml')
+        const expected = [{ "gender": "Male", "id": 10, "location": "Worldender's lair", "name": "Alan Rails", "origin": "unknown", "species": "Human", "status": "Dead", "type": "Superhuman (Ghost trains summoner)" }]
+
+        jest.spyOn(axios, "get").mockResolvedValue({ data: response })
+
+        const result = await RickAndMortyUSA.getCharactersFromXML()
+        expect(result).toMatchObject(expected)
+    })
+})
