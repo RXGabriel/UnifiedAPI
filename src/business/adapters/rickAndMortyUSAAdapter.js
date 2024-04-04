be782c20537ab4ab3818c0f25ccd8ed4f92e5ff0
RXGabriel <gabriel2005rf@gmail.com>
2024-04-04 15:32:47 -0300

    feat: Add adapters for fetching Rick and Morty characters from BRL and USA integrations

---
+++ src/business/adapters/rickAndMortyUSAAdapter.js
@@ -0,0 +1,7 @@
+import RickAndMortyUSA from '../integrations/rickAndMortyUSA.js'
+
+export default class RickAndMortyUSAAdapter {
+    static async getCharacters() {
+        return RickAndMortyUSA.getCharactersFromXML()
+    }
+} 
